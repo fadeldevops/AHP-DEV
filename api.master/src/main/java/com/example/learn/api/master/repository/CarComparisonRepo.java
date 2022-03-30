a3b5fa52881459f00f22b61dcd1579508a2ae976
Fadel Fadell <fadelrahmatullah97@gmail.com>
2022-03-30 09:50:11 +0700

    delet comit meta data

--- api.master/src/main/java/com/example/learn/api/master/repository/CarComparisonRepo.java
+++ api.master/src/main/java/com/example/learn/api/master/repository/CarComparisonRepo.java
@@ -8,8 +8,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface CarComparisonRepo extends JpaRepository<CarComparisonEntity, String> {
 
-   @Query(value = " SELECT car_cd, car_year, car_price, car_age, car_fuel_consumption, car_mileage, car_capacity "
-         + " created_by, created_dt, changed_by, changed_dt "
-         + " FROM public.tb_car_comparison where car_cd = :carCd ", nativeQuery = true)
-   Object[] getCar(@Param("carCd") String carCd)
+      @Query(value = " SELECT car_cd, car_year, car_price, car_age, car_fuel_consumption, car_mileage, car_capacity "
+                  + " created_by, created_dt, changed_by, changed_dt "
+                  + " FROM public.tb_car_comparison where car_cd = :carCd ", nativeQuery = true)
+      Object[] getCar(@Param("carCd") String carCd);
 }
